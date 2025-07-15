0ca88d472b2505b89f78a39de22cbd52301cc24f
Gunasekar862005 <144459580+Gunasekar862005@users.noreply.github.com>
2025-07-15 19:14:15 +0530

    Add files via upload

---
+++ j.js
@@ -0,0 +1,17 @@
+var a = 0;
+
+function aDd(){
+    document.getElementById("h").innerHTML= a++;
+}
+
+function sUb(){
+    document.getElementById("h").innerHTML= a--;
+
+}
+
+
+function reSet(){
+    document.getElementById("h").innerHTML= 0;
+}
+
+
