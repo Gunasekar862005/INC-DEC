3620eeb3ad5a773687eee63a792094fe188f6009
Gunasekar862005 <144459580+Gunasekar862005@users.noreply.github.com>
2025-07-15 19:23:32 +0530

    Update j.js

--- j.js
+++ j.js
@@ -3,15 +3,15 @@ var a = 0;
 function aDd(){
     document.getElementById("h").innerHTML= a++;
 }
-
+aDd();
 function sUb(){
     document.getElementById("h").innerHTML= a--;
 
 }
-
+sUb();
 
 function reSet(){
     document.getElementById("h").innerHTML= 0;
 }
-
+reSet();
 
